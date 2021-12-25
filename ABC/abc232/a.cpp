ed7f109c95093d50b6d5c04dc06259738e7e9731
miiitomi <ytommyrad@gmail.com>
2021-12-25 16:12:55 +0900

    abc232-a

---
+++ ABC/abc232/a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string S;
+    cin >> S;
+    int a = (int)(S[0] - '0');
+    int b = (int)(S[2] - '0');
+    cout << a * b << endl;
+}
