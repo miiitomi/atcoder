d2425fdfc15139079591de4e1661d326d3e24e84
miiitomi <ytommyrad@gmail.com>
2022-02-18 01:06:34 +0900

    modify some setting

--- cpsubmit.sh
+++ cpsubmit.sh
@@ -8,4 +8,4 @@ contest_name=${relative_file_dir_name##*/}
 url=https://atcoder.jp/contests/${contest_name}/tasks/${contest_name}_${problem_name}
 
 # submit
-~/opt/anaconda3/bin/oj submit --yes ${url} ${file}
+~/miniforge3/bin/oj submit --yes ${url} ${file}
--- cptest.sh
+++ cptest.sh
@@ -11,8 +11,8 @@ url=https://atcoder.jp/contests/${contest_name}/tasks/${contest_name}_${problem_
 
 # make sample directory
 if [ ! -e ${test_dir} ]; then
-    ~/opt/anaconda3/bin/oj dl -d ${test_dir} ${url}
+    ~/miniforge3/bin/oj dl -d ${test_dir} ${url}
 fi
 
 # test
-/usr/bin/g++ -std=c++14 ${file} && ~/opt/anaconda3/bin/oj test -c "./a.out" -d ${test_dir}
+/opt/homebrew/bin/g++ -std=c++14 ${file} && ~/miniforge3/bin/oj test -c "./a.out" -d ${test_dir}
