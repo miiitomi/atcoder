1df131a452f89035cd1dd6fca0a2095ee9d01148
miiitomi <ytommyrad@gmail.com>
2023-06-25 00:40:29 +0900

    abc307 c

--- ABC/abc307/c.cpp
+++ ABC/abc307/c.cpp
@@ -1,122 +1,53 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
 using namespace std;
-using namespace atcoder;
-typedef long long ll;
-typedef unsigned long long ull;
-
-void h_w_s_erase(int &h, int &w, vector<string> &S) {
-    bool update = true;
-    while (update) {
-        update = false;
-        
-        bool can_erase = true;
-        for (int j = 0; j < w; j++) {
-            if (S[0][j] == '#') {
-                can_erase = false;
-                break;
-            }
-        }
-        if (can_erase) {
-            S.erase(S.begin());
-            h--;
-            update = true;
-        }
-
-        can_erase = true;
-        for (int j = 0; j < w; j++) {
-            if (S[h-1][j] == '#') {
-                can_erase = false;
-                break;
-            }
-        }
-        if (can_erase) {
-            S.pop_back();
-            h--;
-            update = true;
-        }
-
-        can_erase = true;
-        for (int i = 0; i < h; i++) {
-            if (S[i][0] == '#') {
-                can_erase = false;
-                break;
-            }
-        }
-        if (can_erase) {
-            for (int i = 0; i < h; i++) {
-                S[i].erase(S[i].begin());
-            }
-            w--;
-            update = true;
-        }
-
-        can_erase = true;
-        for (int i = 0; i < h; i++) {
-            if (S[i][w-1] == '#') {
-                can_erase = false;
-                break;
-            }
-        }
-        if (can_erase) {
-            for (int i = 0; i < h; i++) {
-                S[i].pop_back();
-            }
-            w--;
-            update = true;
-        }
-    }
-}
-
 
 int main() {
-    int H_a, W_a, H_b, W_b, H_x, W_x;
-    cin >> H_a >> W_a;
-    vector<string> A(H_a);
-    for (int i = 0; i < H_a; i++) cin >> A[i];
+    int h_a, w_a, h_b, w_b, h_x, w_x;
+    cin >> h_a >> w_a;
+    vector<string> A(h_a);
+    for (int i = 0; i < h_a; i++) cin >> A[i];
+    cin >> h_b >> w_b;
+    vector<string> B(h_b);
+    for (int i = 0; i < h_b; i++) cin >> B[i];
+    cin >> h_x >> w_x;
+    vector<string> X(h_x);
+    for (int i = 0; i < h_x; i++) cin >> X[i];
 
-    cin >> H_b >> W_b;
-    vector<string> B(H_b);
-    for (int i = 0; i < H_b; i++) cin >> B[i];
+    vector<string> Y;
 
-    cin >> H_x >> W_x;
-    vector<string> X(H_x);
-    for (int i = 0; i < H_x; i++) cin >> X[i];
+    for (int i_a = -9; i_a <= 9; i_a++) {
+        for (int j_a = -9; j_a <= 9; j_a++) {
+            for (int i_b = -9; i_b <= 9; i_b++) {
+                for (int j_b = -9; j_b <= 9; j_b++) {
 
-    h_w_s_erase(H_a, W_a, A);
-    h_w_s_erase(H_b, W_b, B);
-    h_w_s_erase(H_x, W_x, X);
+                    Y = vector<string>(h_x, string(w_x, '.'));
+                    bool is_possible = true;
 
-    if (max(H_a, H_b) > H_x || max(W_a, W_b) > W_x) {
-        cout << "No" << endl;
-        return 0;
-    }
-
-    vector<string> Y(H_x, string(W_x, '.'));
-
-    for (int i_a = 0; i_a + H_a <= H_x; i_a++) {
-        for (int j_a = 0; j_a + W_a <= W_x; j_a++) {
-            for (int i_b = 0; i_b + H_b <= H_x; i_b++) {
-                for (int j_b = 0; j_b + W_b <= W_x; j_b++) {
-                    Y = vector<string>(H_x, string(W_x, '.'));
-
-                    for (int i = 0; i < H_a; i++) {
-                        for (int j = 0; j < W_a; j++) {
+                    for (int i = 0; i < h_a; i++) {
+                        for (int j = 0; j < w_a; j++) {
                             if (A[i][j] == '#') {
-                                Y[i_a + i][j_a + j] = '#';
+                                if (0 <= i + i_a && i + i_a < h_x && 0 <= j + j_a && j + j_a < w_x) {
+                                    Y[i + i_a][j + j_a] = '#';
+                                } else {
+                                    is_possible = false;
+                                }
                             }
                         }
                     }
 
-                    for (int i = 0; i < H_b; i++) {
-                        for (int j = 0; j < W_b; j++) {
+                    for (int i = 0; i < h_b; i++) {
+                        for (int j = 0; j < w_b; j++) {
                             if (B[i][j] == '#') {
-                                Y[i_b + i][j_b + j] = '#';
+                                if (0 <= i + i_b && i + i_b < h_x && 0 <= j + j_b && j + j_b < w_x) {
+                                    Y[i + i_b][j + j_b] = '#';
+                                } else {
+                                    is_possible = false;
+                                }
                             }
                         }
                     }
 
-                    if (X == Y) {
+                    if (is_possible && X == Y) {
                         cout << "Yes" << endl;
                         return 0;
                     }
