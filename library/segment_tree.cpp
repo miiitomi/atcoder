0b10984b360f5489094e486a2e008a1d6375c4d7
miiitomi <ytommyrad@gmail.com>
2023-11-13 18:00:12 +0900

    fix library

--- library/segment_tree.cpp
+++ library/segment_tree.cpp
@@ -1,101 +1,52 @@
-// https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_F
+// https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_A
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-const ll INF = (1LL << 31) - 1;
 
-template<typename X, typename M>
-struct LazySegmentTree {
+ll INF = 2147483647;
+
+struct SegmentTree {
     int n;
-    vector<X> data;
-    vector<M> lazy;
-    X X_init = INF; // TODO: Set initial value of data X.
-    M M_init = INF + 1; // TODO: Set initial value of act M.
+    vector<ll> data;
 
-    LazySegmentTree(int n_) {
+    SegmentTree(int _n) {
         n = 1;
-        while (n < n_) n *= 2;
-        data.assign(2*n-1, X_init);
-        lazy.assign(2*n-1, M_init);
+        while (n < _n) n *= 2;
+        data.assign(2*n-1, INF);
     }
 
-    X X_operation(X a, X b) {
-        // TODO: Set operation on X x X. (e.g., min, max, +)
-        return min(a, b);
-    }
-
-    M M_operation(M a, M b) {
-        // TODO: Set operation on M x M.
-        if (b == M_init) return a;
-        else return b;
-    }
-
-    X X_M_operation(X x, M m) {
-        // TODO: Set operation on X x M
-        if (m == M_init) return x;
-        return m;
-    }
-
-    void eval(int k) {
-        if (lazy[k] == M_init) return;
-        if (k < n - 1) {
-            lazy[k * 2 + 1] = M_operation(lazy[k * 2 + 1], lazy[k]);
-            lazy[k * 2 + 2] = M_operation(lazy[k * 2 + 2], lazy[k]);
-        }
-        data[k] = X_M_operation(data[k], lazy[k]);
-        lazy[k] = M_init;
-    }
-
-
-    void _update(int a, int b, M m, int k, int l, int r) {
-        eval(k);
-        if (a <= l && r <= b) {
-            lazy[k] = M_operation(lazy[k], m);
-            eval(k);
-        } else if (a < r && l < b) {
-            _update(a, b, m, k * 2 + 1, l, (l + r) / 2);
-            _update(a, b, m, k * 2 + 2, (l + r) / 2, r);
-            data[k] = X_operation(data[k * 2 + 1], data[k * 2 + 2]);
+    void update(int i, ll a) {
+        i = n - 1 + i;
+        data[i] = a;
+        while (i > 0) {
+            i = (i - 1) / 2;
+            data[i] = min(data[i*2 + 1], data[i*2 + 2]);
         }
     }
 
-    // Update data[i] with i in [a, b) by multiplying m in M.
-    void update(int a, int b, M m) {
-        _update(a, b, m, 0, 0, n);
+    ll query(int i, int j, int k, int l, int r) {
+        if (r <= i || j <= l) return INF;
+        if (i <= l && r <= j) return data[k];
+        ll vl = query(i, j, 2*k + 1, l, (l + r)/2);
+        ll vr = query(i, j, 2*k + 2, (l + r)/2, r);
+        return min(vl, vr);
     }
 
-    X _sub_query(int a, int b, int k, int l, int r) {
-        eval(k);
-        if (r <= a || b <= l) {
-            return X_init;
-        } else if (a <= l && r <= b) {
-            return data[k];
-        } else {
-            X vl = _sub_query(a, b, k * 2 + 1, l, (l + r) / 2);
-            X vr = _sub_query(a, b, k * 2 + 2, (l + r) / 2, r);
-            return X_operation(vl, vr);
-        }
-    }
-
-    // Return the answer in [a, b)
-    X query(int a, int b) {
-        return _sub_query(a, b, 0, 0, n);
+    ll query(int i, int j) {
+        return query(i, j, 0, 0, n);
     }
 };
 
 int main() {
-    int N, Q;
-    cin >> N >> Q;
+    int n, q;
+    cin >> n >> q;
 
-    LazySegmentTree<ll, ll> lst(N);
-    for (int q = 0; q < Q; q++) {
-        ll c, s, t, x;
-        cin >> c >> s >> t;
-        if (c == 0) {
-            cin >> x;
-            lst.update(s, t+1, x);
-        } else {
-            cout << lst.query(s, t+1) << endl;
-        }
+    SegmentTree st(n);
+
+    for (int i = 0; i < q; i++) {
+        int c, x, y;
+        cin >> c >> x >> y;
+        if (c == 0) st.update(x, y);
+        else cout << st.query(x, y+1) << endl;
     }
 }
