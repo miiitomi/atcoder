191fa43111409af1af9aaca6c9421d0c27587a43
miiitomi <ytommyrad@gmail.com>
2024-01-04 01:30:49 +0900

    mv0103r

--- mosoonv/20240103/e.cpp
+++ mosoonv/20240103/e.cpp
@@ -1,63 +1,51 @@
+// https://atcoder.jp/contests/abc310/tasks/abc310_d
 #include <bits/stdc++.h>
 using namespace std;
 
+int ans = 0;
 int N, T, M;
-vector<vector<int>> V;
+vector<vector<int>> G;
 
-void dfs(vector<int> v) {
-    V.push_back(v);
-    int i = T-1;
-    for (int j = v.size()-2; j >= 0; j--) {
-        if (v[j] >= 1) {
-            i = j;
-            break;
+void dfs(int i, vector<int> v) {
+    if (i == N) {
+        if (v.size() == T) ans++;
+        return;
+    }
+
+    for (int j = 0; j < v.size(); j++) {
+        bool ok = true;
+        for (int k : G[i]) {
+            if (v[j] & (1 << k)) {
+                ok = false;
+                break;
+            }
         }
+        if (!ok) continue;
+        vector<int> w = v;
+        w[j] += (1 << i);
+        dfs(i+1, w);
     }
 
-    if (i == T-1) return;
-
-    for (int j = T-1; j > i+1; j--) {
-        v[j-1] += v[j];
-        v[j] = 0;
+    if ((int)v.size() < T) {
+        v.push_back(1 << i);
+        dfs(i+1, v);
     }
-    v[i]--;
-    v[i+1]++;
-    dfs(v);
+    return;
 }
 
 int main() {
     cin >> N >> T >> M;
-
-    vector<int> v(T, 0);
-    v[0] = N-T;
-    dfs(v);
-
-    vector<int> A(M), B(M);
+    G.resize(N);
     for (int i = 0; i < M; i++) {
-        cin >> A[i] >> B[i];
-        A[i]--;
-        B[i]--;
+        int a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        G[a].push_back(b);
+        G[b].push_back(a);
     }
 
-    set<vector<int>> st;
-    for (vector<int> &v : V) {
-        vector<int> w;
-        for (int i = 0; i < T; i++) {
-            w.push_back(i);
-            for (int j = 0; j < v[i]; j++) w.push_back(i);
-        }
+    dfs(0, vector<int>{});
 
-        do {
-            bool ok = true;
-            for (int i = 0; i < M; i++) {
-                if (w[A[i]] == w[B[i]]) {
-                    ok = false;
-                    break;
-                }
-            }
-            if (ok) st.insert(w);
-        } while (next_permutation(w.begin(), w.end()));
-    }
-
-    cout << st.size() << endl;
+    cout << ans << endl;
 }
