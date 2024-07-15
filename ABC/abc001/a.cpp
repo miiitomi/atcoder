034d582107b8dbb2c3ff75d69fa273e94870f6ea
miiitomi <ytommyrad@gmail.com>
2024-07-15 10:15:07 +0000

    abc001 a

---
+++ ABC/abc001/a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = modint998244353;
+const ll INF = 2e+18;
+const ll MOD = 998244353;
+
+void solve() {
+    int H1, H2;
+    cin >> H1 >> H2;
+    cout << H1-H2 << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << fixed << setprecision(15);
+    int T = 1;
+    // cin >> T;
+    while (T--) solve();
+}
