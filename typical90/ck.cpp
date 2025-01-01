d01a8bceae594bf19c49d0c9134ee15b4a17c1dc
miiitomi <ytommyrad@gmail.com>
2025-01-01 10:11:10 +0000

    typical90 bs by ck

---
+++ typical90/ck.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+using mint = modint1000000007;
+const ll INF = 2e+18;
+const ll MOD = 998244353;
+
+template<typename X, typename M> struct LazySegmentTree {
+    int n;
+    vector<X> data;
+    vector<M> lazy;
+    X X_init = 0; // TODO: Set initial value of data X.
+    M M_init = 0; // TODO: Set initial value of act M.
+
+    LazySegmentTree(int n_) {
+        n = 1;
+        while (n < n_) n *= 2;
+        data.assign(2*n-1, X_init);
+        lazy.assign(2*n-1, M_init);
+    }
+
+    X X_operation(X a, X b) {
+        // TODO: Set operation on X x X. (e.g., min, max, +)
+        return a+b;
+    }
+
+    M M_operation(M a, M b) {
+        // TODO: Set operation on M x M.
+        return a+b;
+    }
+
+    X X_M_operation(X x, M m) {
+        // TODO: Set operation on X x M
+        return x+m;
+    }
+
+    void initialize(int i, X x) {
+        i = n - 1 + i;
+        data[i] = x;
+        while (i > 0) {
+            i = (i - 1) / 2;
+            data[i] = X_operation(data[i*2 + 1], data[i*2 + 2]);
+        }
+    }
+
+    void eval(int k) {
+        if (lazy[k] == M_init) return;
+        if (k < n - 1) {
+            lazy[k * 2 + 1] = M_operation(lazy[k * 2 + 1], lazy[k]);
+            lazy[k * 2 + 2] = M_operation(lazy[k * 2 + 2], lazy[k]);
+        }
+        data[k] = X_M_operation(data[k], lazy[k]);
+        lazy[k] = M_init;
+    }
+
+    void _update(int a, int b, M m, int k, int l, int r) {
+        eval(k);
+        if (a <= l && r <= b) {
+            lazy[k] = M_operation(lazy[k], m);
+            eval(k);
+        } else if (a < r && l < b) {
+            _update(a, b, m, k * 2 + 1, l, (l + r) / 2);
+            _update(a, b, m, k * 2 + 2, (l + r) / 2, r);
+            data[k] = X_operation(data[k * 2 + 1], data[k * 2 + 2]);
+        }
+    }
+
+    // Update data[i] with i in [a, b) by multiplying m in M.
+    void update(int a, int b, M m) {
+        _update(a, b, m, 0, 0, n);
+    }
+
+    X _sub_query(int a, int b, int k, int l, int r) {
+        eval(k);
+        if (r <= a || b <= l) {
+            return X_init;
+        } else if (a <= l && r <= b) {
+            return data[k];
+        } else {
+            X vl = _sub_query(a, b, k * 2 + 1, l, (l + r) / 2);
+            X vr = _sub_query(a, b, k * 2 + 2, (l + r) / 2, r);
+            return X_operation(vl, vr);
+        }
+    }
+
+    // Return the answer in [a, b)
+    X query(int a, int b) {
+        return _sub_query(a, b, 0, 0, n);
+    }
+};
+
+
+void solve() {
+    ll N, K;
+    cin >> N >> K;
+    vector<ll> A(N);
+    for (int i = 0; i < N; i++) cin >> A[i];
+    vector<ll> v = A;
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+    for (ll &a : A) a = distance(v.begin(), lower_bound(v.begin(), v.end(), a));
+    LazySegmentTree<mint,mint> dp(N+1);
+    dp.update(0, 1, 1);
+    ll M = v.size();
+    fenwick_tree<ll> ft(M);
+    ll r = 0, tmp = 0;
+    for (ll l = 0; l < N; l++) {
+        while (r < N && tmp + ft.sum(A[r]+1, M) <= K) {
+            tmp += ft.sum(A[r]+1, M);
+            ft.add(A[r], 1);
+            r++;
+        }
+        mint x = dp.query(l, l+1);
+        dp.update(l+1, r+1, x);
+        tmp -= ft.sum(0, A[l]);
+        ft.add(A[l], -1);
+    }
+    mint ans = dp.query(N, N+1);
+    cout << ans.val() << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << fixed << setprecision(15);
+    int T = 1;
+    // cin >> T;
+    while (T--) solve();
+}
